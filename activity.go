61d69c686027023fad45678da995e339776d5211
chintan <chintan.sakhiya@improwised.com>
2023-05-08 16:44:01 +0530

    test:return value false

--- activity.go
+++ activity.go
@@ -22,7 +22,7 @@ func (a *Activity) Metadata() *activity.Metadata {
 
 // Eval implements api.Activity.Eval - Logs the Message
 func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
-
+	return false, nil
 	input := &Input{}
 	err = ctx.GetInputObject(input)
 	if err != nil {
