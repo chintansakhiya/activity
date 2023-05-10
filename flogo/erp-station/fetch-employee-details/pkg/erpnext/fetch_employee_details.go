e43a99ed01927dcc17a9d4e8a90d9228ff5310a3
chintan <chintan.sakhiya@improwised.com>
2023-05-10 12:52:04 +0530

    debug

--- flogo/erp-station/fetch-employee-details/pkg/erpnext/fetch_employee_details.go
+++ flogo/erp-station/fetch-employee-details/pkg/erpnext/fetch_employee_details.go
@@ -9,13 +9,13 @@ import (
 	"github.com/doug-martin/goqu/v9"
 )
 
-func GetDetails(db *goqu.Database,id string) error {
-	fmt.Println("1")
+func GetDetails(db *goqu.Database, id string) error {
+	 
 	req, err := http.NewRequest("GET", fmt.Sprintf("http://10.0.8.190:8090/api/resource/Employee/%s", id), nil)
 	if err != nil {
 		return err
 	}
-	fmt.Println("2")
+	 
 	req.Header.Set("Authorization", "token 472e6bdd5f355a2:a5b7f758c8cfbf0")
 
 	client := &http.Client{}
@@ -23,20 +23,20 @@ func GetDetails(db *goqu.Database,id string) error {
 	if err != nil {
 		return err
 	}
-	fmt.Println("3")
+	 
 
 	defer response.Body.Close()
 	detail, err := io.ReadAll(response.Body)
 	if err != nil {
 		return err
 	}
-	fmt.Println("4")
+	 
 	var employeeDetails EmployeeDetailsList
 	err = json.Unmarshal(detail, &employeeDetails)
 	if err != nil {
 		return err
 	}
-	fmt.Println("5")
+	fmt.Println(employeeDetails.Data)
 	sql, _, err := db.Insert("employee").Rows(
 		goqu.Record{
 			"id":             employeeDetails.Data.ID,
@@ -59,12 +59,12 @@ func GetDetails(db *goqu.Database,id string) error {
 		return err
 
 	}
+	fmt.Println("6")
 
 	_, err = db.Exec(sql)
 	if err != nil {
 		return err
 	}
-	fmt.Println("6")
 
 	return nil
 }
