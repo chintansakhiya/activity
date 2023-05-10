c662ce69999a2f209cb4607fe8bed885516497ad
chintan <chintan.sakhiya@improwised.com>
2023-05-10 11:14:59 +0530

    test

--- flogo/erp-station/fetch-employee-details/activity.go
+++ flogo/erp-station/fetch-employee-details/activity.go
@@ -2,45 +2,57 @@ package fetchemployee
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/project-flogo/core/activity"
 )
 
 func init() {
-	err := activity.Register(&Activity{})
-	if err != nil {
-		log.Fatal(err)
-	}
+	_ = activity.Register(&Activity{}) //activity.Register(&Activity{}, New) to create instances using factory method 'New'
 }
 
-var activityMd = activity.ToMetadata(&Settings{}, &Input{})
+var activityMd = activity.ToMetadata(&Settings{}, &Input{}, &Output{})
 
+// New optional factory method, should be used if one activity instance per configuration is desired
+// func New(ctx activity.InitContext) (activity.Activity, error) {
+
+// 	s := &Settings{}
+// 	err := metadata.MapToStruct(ctx.Settings(), s, true)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	ctx.Logger().Debugf("Setting: %s", s.ASetting)
+
+// 	act := &Activity{} //add aSetting to instance
+
+// 	return act, nil
+// }
+
+// Activity is an sample Activity that can be used as a base to create a custom activity
+ 
+
+// Metadata returns the activity's metadata
 func (a *Activity) Metadata() *activity.Metadata {
 	return activityMd
 }
+
+// Eval implements api.Activity.Eval - Logs the Message
 func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
+
 	input := &Input{}
 	err = ctx.GetInputObject(input)
-
 	if err != nil {
-		return false, err
+		return true, err
 	}
-	fmt.Println(input)
-	// cfg := config.GetConfig()
 
-	// db, err := database.PostgresDBConnection(cfg)
-	// if err != nil {
-	// 	return false, err
-	// }
-	// var id erpnext.EmployeeID
-	// err = json.Unmarshal([]byte(input.employeeId), &id)
-	// if err != nil {
-	// 	return false, err
-	// }
-	// err = erpnext.GetDetails(db, id.EmployeeID)
-	// if err != nil {
-	// 	return false, err
-	// }
+	ctx.Logger().Debugf("Input: %s", input.AnInput)
+	fmt.Println("fron activity 123456", input)
+
+	output := &Output{AnOutput: input.AnInput}
+	err = ctx.SetOutputObject(output)
+	if err != nil {
+		return true, err
+	}
+
 	return true, nil
 }
