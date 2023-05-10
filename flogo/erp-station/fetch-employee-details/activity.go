095702c8c2514b9d6da9be7b654da7e372870e30
chintan <chintan.sakhiya@improwised.com>
2023-05-10 11:56:02 +0530

    unmarshel id

--- flogo/erp-station/fetch-employee-details/activity.go
+++ flogo/erp-station/fetch-employee-details/activity.go
@@ -1,8 +1,13 @@
 package fetchemployee
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 
+	"github.com/chintansakhiya/activity/flogo/erp-station/fetch-employee-details/config"
+	"github.com/chintansakhiya/activity/flogo/erp-station/fetch-employee-details/database"
+	"github.com/chintansakhiya/activity/flogo/erp-station/fetch-employee-details/pkg/erpnext"
 	"github.com/project-flogo/core/activity"
 )
 
@@ -29,7 +34,6 @@ var activityMd = activity.ToMetadata(&Settings{}, &Input{}, &Output{})
 // }
 
 // Activity is an sample Activity that can be used as a base to create a custom activity
- 
 
 // Metadata returns the activity's metadata
 func (a *Activity) Metadata() *activity.Metadata {
@@ -48,10 +52,26 @@ func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
 	ctx.Logger().Debugf("Input: %s", input.EmployeeId)
 	fmt.Println("fron activity chintan", input)
 
-	output := &Output{AnOutput: input.EmployeeId}
-	err = ctx.SetOutputObject(output)
+	// output := &Output{AnOutput: input.EmployeeId}
+	// err = ctx.SetOutputObject(output)
+	// if err != nil {
+	// 	return true, err
+	// }
+	cfg := config.GetConfig()
+
+	db, err := database.PostgresDBConnection(cfg)
 	if err != nil {
-		return true, err
+		return false, err
+	}
+	var id erpnext.EmployeeID
+	err = json.Unmarshal([]byte(input.EmployeeId), &id)
+	if err != nil {
+		return false, err
+	}
+	log.Fatal(id)
+	err = erpnext.GetDetails(db, id.EmployeeID)
+	if err != nil {
+		return false, err
 	}
 
 	return true, nil
